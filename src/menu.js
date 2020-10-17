42f82ca6dad68ef5f2baefe369116b79f7c4a7e3
Jose Pedraza <pastorpedraza3@gmail.com>
2020-10-16 22:03:36 -0500

    fix linters for navbar.js

--- src/menu.js
+++ src/menu.js
@@ -1,15 +1,18 @@
-import img_1 from './images/tacos.png';
-import img_2 from './images/taco-bisteck.png';
-import img_3 from './images/taco-chorizo.png';
-import img_4 from './images/taco-cabeza.png';
-import img_5 from './images/taco-barbacoa.png';
-import img_6 from './images/taco-lengua.png';
+/* eslint-disable brace-style, padded-blocks */
+
+
+import Img1 from './images/tacos.png'
+import Img2 from './images/taco-bisteck.png';
+import Img3 from './images/taco-chorizo.png';
+import Img4 from './images/taco-cabeza.png';
+import Img5 from './images/taco-barbacoa.png';
+import Img6 from './images/taco-lengua.png';
 
 
 const menu = () =>
 {
 
-  const container = document.getElementById("body");
+  const container = document.getElementById('body');
   const menu = document.createElement('div');
   const text = document.createElement('h1');
   const flexbox = document.createElement('div');
@@ -36,12 +39,12 @@ const menu = () =>
   const img5 = document.createElement('img');
   const img6 = document.createElement('img');
 
-  const text_taco1 = document.createElement('span');
-  const text_taco2 = document.createElement('span');
-  const text_taco3 = document.createElement('span');
-  const text_taco4 = document.createElement('span');
-  const text_taco5 = document.createElement('span');
-  const text_taco6 = document.createElement('span');
+  const textTaco1 = document.createElement('span');
+  const textTaco2 = document.createElement('span');
+  const textTaco3 = document.createElement('span');
+  const textTaco4 = document.createElement('span');
+  const textTaco5 = document.createElement('span');
+  const textTaco6 = document.createElement('span');
 
   container.appendChild(menu);
   menu.appendChild(text);
@@ -57,43 +60,43 @@ const menu = () =>
 
   taco1.appendChild(img1);
   taco1.appendChild(newline);
-  taco1.appendChild(text_taco1);
+  taco1.appendChild(textTaco1);
 
   taco2.appendChild(img2);
   taco2.appendChild(newline2);
-  taco2.appendChild(text_taco2);
+  taco2.appendChild(textTaco2);
 
   taco3.appendChild(img3);
   taco3.appendChild(newline3);
-  taco3.appendChild(text_taco3);
+  taco3.appendChild(textTaco3);
 
   taco4.appendChild(img4);
   taco4.appendChild(newline4);
-  taco4.appendChild(text_taco4);
+  taco4.appendChild(textTaco4);
 
   taco5.appendChild(img5);
   taco5.appendChild(newline5);
-  taco5.appendChild(text_taco5);
+  taco5.appendChild(textTaco5);
 
   taco6.appendChild(img6);
   taco6.appendChild(newline6);
-  taco6.appendChild(text_taco6);
+  taco6.appendChild(textTaco6);
 
   text.setAttribute('class', 'center-text');
   flexbox.setAttribute('class', 'flex container-align');
   flexbox2.setAttribute('class', 'flex container-align');
-  img1.setAttribute('src', img_1);
+  img1.setAttribute('src', Img1);
   img1.setAttribute('class', 'menu-img');
   img2.setAttribute('class', 'menu-img');
-  img2.setAttribute('src', img_2);
+  img2.setAttribute('src', Img2);
   img3.setAttribute('class', 'menu-img');
-  img3.setAttribute('src', img_3);
+  img3.setAttribute('src', Img3);
   img4.setAttribute('class', 'menu-img');
-  img4.setAttribute('src', img_4);
+  img4.setAttribute('src', Img4);
   img5.setAttribute('class', 'menu-img');
-  img5.setAttribute('src', img_5);
+  img5.setAttribute('src', Img5);
   img6.setAttribute('class', 'menu-img');
-  img6.setAttribute('src', img_6);
+  img6.setAttribute('src', Img6);
   taco1.setAttribute('class', 'center-text');
   taco2.setAttribute('class', 'center-text');
   taco3.setAttribute('class', 'center-text');
@@ -101,14 +104,14 @@ const menu = () =>
   taco5.setAttribute('class', 'center-text');
   taco6.setAttribute('class', 'center-text');
 
-  text.textContent = "Tacos";
-  text_taco1.textContent = "Tacos al Pastor";
-  text_taco2.textContent = "Tacos al Bisteck";
-  text_taco3.textContent = "Tacos de Chorizo";
-  text_taco4.textContent = "Tacos de Cabeza";
-  text_taco5.textContent = "Tacos de Barbacoa";
-  text_taco6.textContent = "Tacos de Lengua";
+  text.textContent = 'Tacos';
+  textTaco1.textContent = 'Tacos al Pastor';
+  textTaco2.textContent = 'Tacos al Bisteck';
+  textTaco3.textContent = 'Tacos de Chorizo';
+  textTaco4.textContent = 'Tacos de Cabeza';
+  textTaco5.textContent = 'Tacos de Barbacoa';
+  textTaco6.textContent = 'Tacos de Lengua';
 
-}
+};
 
 export default menu;
